b88ad65a68a08aeba556cb463e2373c02356faa9
SelfishSquirrel <148039970+SelfishSquirrel@users.noreply.github.com>
2025-12-16 13:46:09 +0100

    Console

---
+++ Szyfr/Szyfr/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+class MainProgram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj tekst do zaszyfrowania:");
+        string text = Console.ReadLine();
+        Console.WriteLine("Podaj klucz szyfrowania (liczba całkowita):");
+        int mover = int.Parse(Console.ReadLine());
+        Console.WriteLine(MakeEncode(text, mover));
+    }
+
+    private static string MakeEncode(string text, int mover)
+    {
+        StringBuilder cypherText = new StringBuilder();
+        char[] chars = text.ToCharArray();
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            char currentChar = chars[i];
+            int code = (int)currentChar;
+
+            if (!((code >= 65 && code <= 90) || (code >= 97 && code <= 122)))
+            {
+                return "Błąd: tekst zawiera niedozwolone znaki.";
+            }
+
+            int baseCode = (code >= 65 && code <= 90) ? 65 : 97; 
+            int shifted = (code - baseCode + mover) % 26;
+
+            if (shifted < 0)
+            {
+                shifted += 26;
+            }
+
+            cypherText.Append((char)(baseCode + shifted));
+        }
+
+        return cypherText.ToString();
+    }
+}
